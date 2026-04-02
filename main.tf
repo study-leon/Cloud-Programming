90c09a068949725407b3ba8b018fde4ae7e7eaac
b-leon18 <leon.beez@iu-study.org>
2026-04-02 12:24:10 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -67,12 +67,3 @@ resource "aws_s3_bucket_policy" "website" {
     ]
   })
 }
-
-resource "aws_s3_bucket_public_access_block" "website" {
-  bucket = aws_s3_bucket.website.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
