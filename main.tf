5f186c5c371288b51d0bf8b525ab8e845184b757
b-leon18 <leon.beez@iu-study.org>
2026-04-02 12:00:38 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -51,3 +51,28 @@ resource "aws_cloudfront_distribution" "website" {
     cloudfront_default_certificate = true
   }
 }
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
